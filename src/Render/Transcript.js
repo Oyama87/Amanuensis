0049c61fd495ad7e6a0abf6db4032d69f492d8ad
Justin Taylor Goldberg <JustinTaylorG@Gmail.com>
2019-01-19 15:58:49 -0500

    On opening audio file, transcription begins on GUI screen in real time (throttled), and timestamps recorded on hash table.

--- src/Render/Transcript.js
+++ src/Render/Transcript.js
@@ -2,18 +2,6 @@ import React, { Component } from 'react'
 // const { ipcRenderer } = window.require('electron')
 
 export default class Transcript extends Component {
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     transcriptText: ''
-  //   }
-  //   ipcRenderer.on('load-transcript', (event, text) => {
-  //     // console.log('load-transcript callback')
-  //     this.setState({
-  //       transcriptText: text
-  //     })
-  //   })
-  // }
   
   render() {
     return (
